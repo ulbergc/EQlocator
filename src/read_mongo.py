0e2d62b79d65cb06432cb9bedc835fd8d24cde86
ulbergc <carlulberg@gmail.com>
2019-09-22 10:07:16 -0700

    add spark migrate code

---
+++ src/read_mongo.py
@@ -0,0 +1,23 @@
+
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+
+def read_data(spark):
+    df = spark.read \
+        .format("mongo") \
+        .load()
+    df.printSchema()
+
+
+if __name__ == "__main__":
+    master_DNS="http://ec2-34-223-143-198.us-west-2.compute.amazonaws.com"
+    spark = SparkSession \
+        .builder \
+        .appName("MigrateData") \
+        .master("spark://" + master_DNS + ":7077")
+        .config("spark.mongodb.input.uri","mongodb://10.0.0.4/dbsmall.movie")
+        .getOrCreate()
+
+    read_data(spark)
+
+    spark.stop()
