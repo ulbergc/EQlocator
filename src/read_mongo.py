e3feec26ba7b86d9776ec0ea6006308dce3a002b
ulbergc <carlulberg@gmail.com>
2019-09-22 10:54:46 -0700

    add mongo package to read_mongo.py

--- src/read_mongo.py
+++ src/read_mongo.py
@@ -10,11 +10,12 @@ def read_data(spark):
 
 
 if __name__ == "__main__":
-#    master_DNS="http://ec2-34-223-143-198.us-west-2.compute.amazonaws.com"
+#    master_DNS="ec2-34-223-143-198.us-west-2.compute.amazonaws.com"
     spark = SparkSession \
         .builder \
         .appName("MigrateData") \
         .config("spark.mongodb.input.uri","mongodb://10.0.0.4/dbsmall.movie") \
+        .config("spark.jars.packages","org.mongodb.spark:mongo-spark-connector_2.11:2.4.1")
         .getOrCreate()
     
     
