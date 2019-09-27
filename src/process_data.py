c9e925f5ba295cc199dd696022549d984485f5dc
Ubuntu <ubuntu@ip-10-0-0-13.us-west-2.compute.internal>
2019-09-27 16:51:32 +0000

    mv read_mongo2.py to process_data.py

--- src/process_data.py
+++ src/process_data.py
@@ -3,6 +3,7 @@ from pyspark.sql.types import *
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
 import time
+import sys
 
 def read_data(spark):
     df = spark.read \
@@ -14,60 +15,54 @@ def read_data(spark):
 
 
 def write_data(df):
-#    df.write.format('jdbc').options(
-#        url='jdbc:mysql://localhost/database_name',
-#        driver='com.mysql.jdbc.Driver',
-#        dbtable='DestinationTableName',
-#        user='your_user_name',
-#        password='your_password').mode('append').save()
-
     #data_to_write=df.select("_id","Latitude","Longitude")
-   # data_to_write=df.select("value","extra")
     data_to_write=df
     #data_to_write.show()
-    
 
     # check that database is created and table is not
+
+    # define 
     mysqlhost="10.0.0.11:3306/"
-    mysqldb="large2"
+    mysqldb="tmp4"
+    mysqltable="table1"
     mysqluserpw="?user=user&password=pw"
     data_to_write.write.format("jdbc").options(
         url=("jdbc:mysql://" + mysqlhost + mysqldb + mysqluserpw),
         driver="com.mysql.cj.jdbc.Driver",
-        dbtable="table1").save()
+        dbtable=mysqltable).save()
 
     # could add .mode("append") before save to append, otherwise creates new
 
-def locateEQ(x,boundingBox):
+def locateEQ(x,sta,boundingBox):
     print("Processing event {evid}, {nobs} observations".format(evid=x._id,nobs=len(x.Observations)))
     #print(type(x))
+
+    # default values
     idtmp=x._id
     lattmp=float(x.Latitude)
     lontmp=float(x.Longitude)
-#    if len(str(lattmp))<3:
-#	print("*******ev {}, lat='''{}'''".format(x._id,lattmp))
-#	lattmp=46.2
-#    if len(str(lontmp))<3:
-#	lontmp=-121.6
-#        print("*******ev {}, lon='''{}'''".format(x._id,lontmp))
-#    f=open("../logs/sparklogtmp.log","a")
-#    f.write("{} {} {}\n".format(x._id,lattmp,lontmp))
-#    f.close()
-    #print(x)
-    #print((float(x.Latitude) + float(x.Longitude))/2)
-    #return (x._id,float(x.Latitude),float(x.Longitude))
-#    idtmp=100
-#    lattmp=46.5
-#    lontmp=-122
+    # check if Observations exists, if so, load it
+    obsName="Observations"
+    if obsName in x:
+	obs=x[obsName]
+	print(type(obs))  
+	lattmp=obs[0]["Uncertainty"]
+
+
     return (idtmp,lattmp,lontmp)
 
 if __name__ == "__main__":
 #    master_DNS="ec2-34-223-143-198.us-west-2.compute.amazonaws.com"
-    dbname="large2" # small-tmp1; large-tmp0
-    collname="c0" # small-c0; large-c0
+
+    if len(sys.argv) != 3:
+	print("Usage: process_data.py <dbname> <tablename>")
+	sys.exit(-1)
+
+    dbname=sys.argv[1] # small-tmp1; large-tmp0
+    collname=sys.argv[2] # small-c0; large-c0
     spark = SparkSession \
         .builder \
-        .appName("MigrateData") \
+        .appName("ProcessData") \
         .config("spark.mongodb.input.uri","mongodb://10.0.0.5/{db}.{col}".format(db=dbname,col=collname)) \
         .config("spark.jars.packages","org.mongodb.spark:mongo-spark-connector_2.11:2.4.1") \
         .getOrCreate()
@@ -81,24 +76,22 @@ if __name__ == "__main__":
     
     # read station information
     sta=spark.read.format("mongo").option("uri","mongodb://10.0.0.5/{db}.{col}".format(db=dbname,col="sta")).load()   
+    print("{} stations".format(sta.count()))
+    
     # get min/max location values 
     print(sta.printSchema())
     lat_min=sta.agg({"Latitude": "min"}).head()[0]
     lat_max=sta.agg({"Latitude": "max"}).head()[0]
     lon_min=sta.agg({"Longitude": "min"}).head()[0]
     lon_max=sta.agg({"Longitude": "max"}).head()[0]
-#    min_max=sta.agg(min("Latitude"),max("Latitude"),min("Longitude"),max("Longitude")).head()
- #   lat_min=min_max.getInt(0)
- #   lat_max=min_max.getInt(1)
-#    lon_min=min_max.getInt(2)
-#    lon_max=min_max.getInt(3)
     print('*** Station bounding box= [{:.4f} {:.4f} {:.4f} {:.4f}]'.format(lat_min,lat_max,lon_min,lon_max))
     print('*** lat_min={:.4f}'.format(lat_min))
+
    # process each record
     #df.foreach(lambda x: locateEQ(x,'fe'))
     
     # use map because it will return a transformed rdd
-#    d_rdd=df.rdd.map(lambda x: locateEQ(x,[lat_min,lat_max,lon_min,lon_max]))
+    d_rdd=df.rdd.map(lambda x: locateEQ(x,sta,[lat_min,lat_max,lon_min,lon_max]))
     #print(d_rdd.count())
 
     # drop this from memory, don't need it
@@ -109,19 +102,19 @@ if __name__ == "__main__":
             StructField("Longitude", FloatType(), True)]
     sql_schema = StructType(fields)
 
-    #d_rdd_with_schema = sqlContext.applySchema(d_rdd,sql_schema)
-#    df_out=spark.createDataFrame(d_rdd,sql_schema)
+#    d_rdd_with_schema = sqlContext.applySchema(d_rdd,sql_schema)
+    df_out=spark.createDataFrame(d_rdd,sql_schema)
     #df_out=spark.createDataFrame(d_rdd)
 
-#    df_out.printSchema()
-    df_out=df.select("_id","Latitude","Longitude")
+    df_out.printSchema()
+    
+    # simple df_out for testing
+#    df_out=df.select("_id","Latitude","Longitude")
     #print(d_rdd.first()[0])
     ##df2.printSchema()
-    # Create table, headers for MySQL
-#    nevent=df_out.count()
-    print("{} stations".format(sta.count()))
-    #print("Sta box: [{} {} {} {}]".format(
-#    sta.describe(['Latitude']).show()
+    # Create table, headers for MySQL?
+    #?? 
+    
     # Write data to MySQL
     write_data(df_out)
 
@@ -130,6 +123,3 @@ if __name__ == "__main__":
     endtime=time.time()
     print("It took {:.1f} seconds".format(endtime-starttime))
 
-#    f=open("../logs/sparklog2.log","a+")
-#    f.write("{:.1f} sec | {} records\n".format(endtime-starttime,nevent))
-#    f.close()
--- src/runSpark.sh
+++ src/runSpark.sh
@@ -5,6 +5,10 @@ if [ $narg -lt 1 ]; then
   exit
 fi
 
+read_db_name=tmp4
+read_table_name=c0
+
+
 starttime=`date +%s`
 PACKAGE=$(echo "--packages org.mongodb.spark:mongo-spark-connector_2.11:2.4.1,mysql:mysql-connector-java:8.0.17")
 
@@ -13,12 +17,12 @@ MASTER=$(echo "--master spark://$masterDNS:7077")
 #MASTER=
 echo $MASTER
 
-echo "Calling spark-submit $PACKAGE $MASTER $1"
+echo "Calling spark-submit $PACKAGE $MASTER $1 $read_db_name $read_table_name"
 
-spark-submit $PACKAGE $MASTER $1
+spark-submit $PACKAGE $MASTER $1 $read_db_name $read_table_name
 
 endtimeFull=`date`
 endtime=`date +%s`
 runtime=$((endtime-starttime))
 echo "Testing"
-echo "$endtimeFull | $runtime | spark-submit $PACKAGE $MASTER $1" >> ../logs/sparktime.log
+echo "$read_db_name | $read_table_name | $endtimeFull | $runtime | spark-submit $PACKAGE $MASTER $1" >> ../logs/sparktime.log
