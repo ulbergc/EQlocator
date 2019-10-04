4b54fa0c508d0eab0737e3e4be12c0ebf51067aa
Ubuntu <ubuntu@ip-10-0-0-4.us-west-2.compute.internal>
2019-10-04 19:56:33 +0000

    editing dashboard

--- Dash/frontend.py
+++ Dash/frontend.py
@@ -249,4 +249,4 @@ def render_content(tab):
 
 #this is the actual server call
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='0.0.0.0', port='5000')
